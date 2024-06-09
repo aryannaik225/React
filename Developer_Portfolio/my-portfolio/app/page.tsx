bf27b00365a3e505407fb6aae71781ce80af8b00
aryannaik225 <aryann2203@gmail.com>
2024-06-09 14:52:32 +0530

    testimonials, clients, and about me four cards done

--- Developer_Portfolio/my-portfolio/app/page.tsx
+++ Developer_Portfolio/my-portfolio/app/page.tsx
@@ -4,16 +4,22 @@ import { FaHome } from "react-icons/fa";
 import Grid from "@/components/Grid";
 import RecentProjects from "@/components/RecentProjects";
 import { navItems } from "@/data";
+import Clients from "@/components/Clients";
+import Experience from "@/components/Experience";
+import Approach from "@/components/Approach";
 
 export default function Home() {
   return (
     <main className="relative font-extralight bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-          <FloatingNav navItems={navItems}/>
+        <FloatingNav navItems={navItems}/>
         <Hero />
         {/* We basically called the Hero.tsx component in here */}
         <Grid />
         <RecentProjects />
+        <Clients />
+        <Experience />
+        <Approach />
       </div>
     </main>
   );
